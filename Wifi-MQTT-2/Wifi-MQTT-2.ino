31d9df03a792daf66afa8d8f2e05581d1bae99e6
TONG JONG <JONGTO@tdm.local>
2025-03-12 17:26:54 +0800

    mqtt publish messages

---
+++ Wifi-MQTT-2/Wifi-MQTT-2.ino
@@ -0,0 +1,184 @@
+#include <WiFi.h>
+#include <Adafruit_MQTT.h>
+#include <Adafruit_MQTT_Client.h>
+#include <format>
+#include <iostream>
+#include <string>
+import std;
+
+/**
+ * Define Macros for constant values
+ *
+ * - MQTT Connection details
+ * - Retry values
+ * - Component Pins
+ */
+#define MQTT_USERNAME "Not_Needed"
+#define MQTT_PASSWORD "Not_Needed"
+#define MQTT_SERVER "10.25.35.123"
+#define MQTT_SERVERPORT 1883
+
+#define RETRY_PERIOD 1000
+#define RETRY_ADJUSTMENT 500
+#define MAX_ATTEMPTS 5
+
+#define LED_RED_1 25
+#define LED_GREEN_1 26
+#define LED_BLUE_1 27
+#define BUTTON 21
+
+#define DEBUG true
+
+
+const char* ssid = "NMT-IoT";
+const char* password = "Its!Working";
+const char* topic = "Button State Topic";
+
+int buttonState = LOW;
+int lastButtonState = LOW;
+
+WiFiClient client;
+Adafruit_MQTT_Client mqtt(&client, MQTT_SERVER, MQTT_SERVERPORT, MQTT_USERNAME, MQTT_PASSWORD);
+Adafruit_MQTT_Subscribe buttonStateSubscription = Adafruit_MQTT_Subscribe(&mqtt, topic);
+
+
+void setup() {
+  if (DEBUG) {
+    Serial.begin(115200);
+  }
+
+  pinMode(LED_RED_1, OUTPUT);
+  pinMode(LED_GREEN_1, OUTPUT);
+  pinMode(LED_BLUE_1, OUTPUT);
+  pinMode(BUTTON, INPUT);
+
+  digitalWrite(LED_RED_1, HIGH);
+  digitalWrite(LED_GREEN_1, LOW);
+  digitalWrite(LED_BLUE_1, LOW);
+
+  bool wiFiConnected = wiFiConnect();
+
+  if (wiFiConnected) {
+    if (DEBUG) {
+      Serial.println("WiFi Connected.");
+    }
+
+    digitalWrite(LED_RED_1, LOW);
+    digitalWrite(LED_GREEN_1, HIGH);
+    digitalWrite(LED_BLUE_1, LOW);
+    wiFiDetails();
+
+    bool mqttConnected = mqttConnect();
+
+    if (mqttConnected) {
+      if (DEBUG) {
+        Serial.println("MQTT Connected.");
+      }
+
+      digitalWrite(LED_BLUE_1, HIGH);
+      delay(RETRY_PERIOD);
+      digitalWrite(LED_BLUE_1, LOW);
+    } else {
+      if (DEBUG) {
+        Serial.println("MQTT Connection Failed.");
+      }
+
+      digitalWrite(LED_RED_1, HIGH);
+      digitalWrite(LED_GREEN_1, LOW);
+      digitalWrite(LED_BLUE_1, LOW);
+    }
+
+  } else {
+    if (DEBUG) {
+      Serial.println("WiFi Connection Failed.");
+    }
+  }
+
+  Serial.print("buttonStage: "); Serial.println(buttonState);
+}
+
+void loop() {
+  
+  buttonState = digitalRead(BUTTON); 
+
+  if (buttonState != lastButtonState) { 
+
+      if (buttonState == LOW) {
+      digitalWrite(LED_BLUE_1, HIGH);
+      Serial.println("pressed");
+      mqtt.publish(topic, std::format("{'name': 'tong', 'button state': {}}", buttonState));
+      // delay(1500);
+    } else {
+      digitalWrite(LED_BLUE_1, LOW);
+      mqtt.publish(topic, "0");
+      Serial.println("not pressed");
+      // delay(1500);
+    }
+    
+    lastButtonState = buttonState;
+  }
+}
+
+bool wiFiConnect() {
+  int attempts = 0;
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  if (DEBUG) {
+    Serial.println("Connecting to WiFi...");
+  }
+
+  while (WiFi.status() != WL_CONNECTED && attempts < MAX_ATTEMPTS) {
+
+    if (DEBUG) {
+      Serial.print(".");
+    }
+
+    delay(RETRY_PERIOD + RETRY_ADJUSTMENT * attempts);
+    attempts++;
+  }
+
+  return attempts < MAX_ATTEMPTS;
+}
+
+bool mqttConnect() {
+
+  uint8_t attempts = 0;
+  int8_t mqttConnectionResult;
+
+  if (DEBUG) {
+    Serial.println();
+    Serial.println("Connecting to MQTT Broker/Server...");
+  }
+
+  while ((mqttConnectionResult = mqtt.connect()) != 0 && attempts < MAX_ATTEMPTS) {
+
+    if (DEBUG) {
+      Serial.print(".");
+    }
+
+    delay(RETRY_PERIOD + RETRY_ADJUSTMENT * attempts);
+    attempts++;
+
+    if (mqttConnectionResult != 0) {
+      mqtt.disconnect();
+    }
+  }
+
+  return attempts < MAX_ATTEMPTS;
+}
+
+void wiFiDetails() {
+  if (DEBUG) {
+    Serial.println();
+    Serial.print("Local IP:        ");
+    Serial.println(WiFi.localIP());
+    Serial.print("Gateway IP:      ");
+    Serial.println(WiFi.gatewayIP());
+    Serial.print("Signal Strength: ");
+    Serial.println(WiFi.RSSI());
+    Serial.print("Hostname:        ");
+    Serial.println(WiFi.getHostname());
+  }
+}
--- WifiScanner/WifiScanner.ino
+++ WifiScanner/WifiScanner.ino
@@ -1,39 +1,96 @@
 #include <WiFi.h>
+#include <Adafruit_MQTT.h>
+#include <Adafruit_MQTT_Client.h>
 
-#define LED_RED 19
-#define LED_BLUE 18
+#define MQTT_USERNAME "Not_Needed"
+#define MQTT_PASSWORD "Not_Needed"
+#define MQTT_SERVER "10.25.35.123"
+#define MQTT_SERVERPORT 1883
+
+#define LED_RED 25
+#define LED_BLUE 27
+#define LED_GREEN 26
+#define PUSH_BUTTON 18
+
 #define MAX_ATTEMPTS 5
+#define RETRY_PERIOD 1000
+#define RETRY_ADJUSTMENT 500
 
 const char* SSID = "NMT-IoT";
 const char* password = "Its!Working";
 
-bool wifiConnection = false;
+bool buttonState = LOW; 
+
+WiFiClient client;
+Adafruit_MQTT_Client mqtt(&client, MQTT_SERVER, MQTT_SERVERPORT, MQTT_USERNAME, MQTT_PASSWORD);
 
 void setup() {
   Serial.begin(115200);
 
   pinMode(LED_RED, OUTPUT);
   pinMode(LED_BLUE, OUTPUT);
+  pinMode(LED_GREEN, OUTPUT);
+  pinMode(PUSH_BUTTON, INPUT);
 
   digitalWrite(LED_RED, HIGH);
   digitalWrite(LED_BLUE, LOW);
+  digitalWrite(LED_GREEN, LOW);
 
-  bool connected = wifiConnect();
+  bool wifiConnected = wifiConnect();
 
-  if (connected) {
-    Serial.println("Connected successfully.");
-    wifiConnection = true;
+  if (wifiConnected) {
+    Serial.println("Wifi connected successfully.");
+    digitalWrite (LED_RED, LOW);
+    digitalWrite (LED_GREEN, HIGH);
+    wiFiDetails();
   } else {
-    Serial.println("Connection failed.");
-    wifiConnection = false;
+    Serial.println("Wifi connection failed.");
+  }
+
+  bool mqttConnected = mqttConnect();
+  if (mqttConnected) {
+    digitalWrite(LED_RED, HIGH);
+    Serial.println("MQTT connected successfully.");
+    delay(RETRY_PERIOD);
+    digitalWrite(LED_RED, LOW);
+  } else {
+    digitalWrite(LED_RED, HIGH);
+    digitalWrite(LED_GREEN, LOW);
+    Serial.println("MQTT connection failed.");
   }
 }
 
 void loop() {
- while (wifiConnection){
-  digitalWrite(LED_RED, LOW);
-  digitalWrite(LED_BLUE, HIGH);
- }
+  buttonState = digitalRead(PUSH_BUTTON);
+  if (buttonState == HIGH) {
+    Serial.print("1");
+    digitalWrite(LED_RED, HIGH);
+    digitalWrite(LED_GREEN, LOW);
+  } else {
+    Serial.print("0");
+    digitalWrite(LED_GREEN, HIGH);
+    digitalWrite(LED_RED, LOW);
+  }
+}
+
+bool mqttConnect() {
+  uint8_t attempts = 0;
+  int8_t mqttConnectionResult;
+  
+  Serial.println();
+  Serial.println("Connecting to MQTT broker...");
+
+  while((mqttConnectionResult = mqtt.connect()) != 0 && attempts < MAX_ATTEMPTS) {
+    Serial.print(".");
+    delay(RETRY_PERIOD + RETRY_ADJUSTMENT * attempts);
+    attempts++;
+
+    if (mqttConnectionResult != 0) {
+      mqtt.disconnect();
+    }
+  }
+
+  return attempts < MAX_ATTEMPTS;
 }
 
 bool wifiConnect() {
@@ -51,4 +108,16 @@ bool wifiConnect() {
   }
 
   return attempts < MAX_ATTEMPTS;
+}
+
+void wiFiDetails() {
+  Serial.println();
+  Serial.print("Local IP:        ");
+  Serial.println(WiFi.localIP());
+  Serial.print("Gateway IP:      ");
+  Serial.println(WiFi.gatewayIP());
+  Serial.print("Signal Strength: ");
+  Serial.println(WiFi.RSSI());
+  Serial.print("Hostname:        ");
+  Serial.println(WiFi.getHostname());
 }
